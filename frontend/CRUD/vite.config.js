47f9a2e3797088ee29cf146da87cd8f2dc969a31
FaiTh725 <sasha.zelenukho.2016@mail.ru>
2025-01-26 17:45:38 +0300

    Implement nginx with frontend app web service without fixing fronend

--- frontend/CRUD/vite.config.js
+++ frontend/CRUD/vite.config.js
@@ -1,14 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import mkcert from 'vite-plugin-mkcert'
+// import mkcert from 'vite-plugin-mkcert'
+
+const sslKey = process.env.VITE_APP_SSL_KEY_FILE_PATH;
+const sslSert = process.env.VITE_APP_SSL_CRT_FILE_PATH;
+
+console.log(sslKey);
+console.log(sslSert);
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), mkcert()],
+  plugins: [react()],
   server: {
     https: true,
     host: true,
     strictPort: true,
     port: 5173,
+  },
+  build: {
+    outDir: "dist"
+  },
+  https: {
+    key: sslKey,
+    sert: sslSert
   }
 })
